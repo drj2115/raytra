f82d144c46741ce0af21237d3eadd5eb560b5506
Dallas Jones <dallas@fckyoself.limp.noodle>
2016-07-16 22:41:52 -0400

    minor formatting in light.h

--- light.h
+++ light.h
@@ -16,11 +16,11 @@ class Light {
 		const Vec &v2 = Vec(),
 		const Vec &v3 = Vec(),
 		const double &l = 0)
-	{ type = t; v = v1; rgb = color; n = v2; u = v3; len = l; }
+		{ type = t; v = v1; rgb = color; n = v2; u = v3; len = l; }
 
 	~Light(void) {};
 
-	Vec get_rgb(void) const {return rgb;}
+	Vec get_rgb(void) const { return rgb; }
 
         Vec v, rgb, n, u; /* note: n, u, and len used only for area lights */
 	double len;
